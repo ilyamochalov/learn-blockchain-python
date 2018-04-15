8c1c177ea89d55f03d60c8fc06e400cea3d162a8
Ilya Mochalov <2111311792@qq.com>
2018-04-15 12:57:28 +0800

    Added simple consensus

--- flask_app.py
+++ flask_app.py
@@ -39,7 +39,6 @@ def mine():
         'previous_hash': block['previous_hash'],
     }
 
-
     return jsonify(response), 200
 
 
@@ -63,6 +62,42 @@ def full_chain():
         'chain': blockchain.chain,
         'length': len(blockchain.chain)
     }
+
+    return jsonify(response), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
 
     return jsonify(response), 200
 
